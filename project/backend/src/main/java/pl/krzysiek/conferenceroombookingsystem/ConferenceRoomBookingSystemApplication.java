c13ce40c21884f00b3044a75c56bb601a692cea7
KS-coder-98 <krzysztofsekowski1998@gmail.com>
2021-10-19 13:30:53 +0200

    init backend project

---
+++ project/backend/src/main/java/pl/krzysiek/conferenceroombookingsystem/ConferenceRoomBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.krzysiek.conferenceroombookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRoomBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceRoomBookingSystemApplication.class, args);
+    }
+
+}
---
+++ project/backend/src/test/java/pl/krzysiek/conferenceroombookingsystem/ConferenceRoomBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krzysiek.conferenceroombookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceRoomBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
