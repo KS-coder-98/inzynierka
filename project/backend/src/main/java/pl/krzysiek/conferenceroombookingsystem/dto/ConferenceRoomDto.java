fe97c6802863e7c585d0f4dae201fa2765440e84
KS-coder-98 <krzysztofsekowski1998@gmail.com>
2021-11-16 02:53:11 +0100

    16.11.2021 add EquipmentDto to ConferenceRoomDto

--- project/backend/src/main/java/pl/krzysiek/conferenceroombookingsystem/dto/ConferenceRoomDto.java
+++ project/backend/src/main/java/pl/krzysiek/conferenceroombookingsystem/dto/ConferenceRoomDto.java
@@ -2,10 +2,13 @@ package pl.krzysiek.conferenceroombookingsystem.dto;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class ConferenceRoomDto {
     private long id;
     private String name;
     private String description;
     private int capacity;
+    private Set<EquipmentDto> equipment;
 }
