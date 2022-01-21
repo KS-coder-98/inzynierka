8262057081ed8503e666945bdc34b6ac75730c3f
KS-coder-98 <krzysztofsekowski1998@gmail.com>
2022-01-21 14:51:20 +0100

    21.01 fix typo in db

--- project/backend/src/main/resources/data.sql
+++ project/backend/src/main/resources/data.sql
@@ -3,28 +3,28 @@ VALUES (22, 'przytulna', 'A1'),
        (50, 'ciemna', 'A2'),
        (30, 'przechodnia', 'A3'),
        (40, 'luksusowa', 'A4'),
-       (20, 'glowan', 'A5'),
+       (20, 'główna', 'A5'),
        (150, 'bardzo duża', 'A6'),
-       (200, 'najwieksza', 'A7');
+       (200, 'największa', 'A7');
 
 INSERT INTO conference_rooms_booking.equipment (description, name, conference_room_id)
 VALUES ('i5 8GB ramu', 'komputer stacjonarny', 1),
-       ('70 calowy', 'żutnik', 1),
-       ('na krede', 'tablica', 1),
+       ('70 calowy', 'rzutnik', 1),
+       ('na kredę', 'tablica', 1),
        ('i7 18GB ramu', 'komputer stacjonarny', 2),
-       ('100 calowy', 'żutnik', 2),
+       ('100 calowy', 'rzutnik', 2),
        ('szara', 'tablica', 2),
        ('i5 8GB ramu', 'komputer stacjonarny', 3),
-       ('na krede', 'tablica', 3),
+       ('na kredę', 'tablica', 3),
        ('i3 3GB ramu', 'komputer stacjonarny', 4),
-       ('70 calowy', 'żutnik', 4),
-       ('na krede', 'tablica', 4),
+       ('70 calowy', 'rzutnik', 4),
+       ('na kredę', 'tablica', 4),
        ('i9 32GB ramu', 'komputer stacjonarny', 5),
-       ('170 calowy', 'żutnik', 5),
-       ('na krede', 'tablica', 5),
+       ('170 calowy', 'rzutnik', 5),
+       ('na kredę', 'tablica', 5),
        ('i5 8GB ramu', 'komputer stacjonarny', 7),
-       ('70 calowy', 'żutnik', 7),
-       ('na krede', 'tablica', 7);
+       ('70 calowy', 'rzutnik', 7),
+       ('na kredę', 'tablica', 7);
 
 INSERT INTO conference_rooms_booking.user_contact (city, house_number, phone_area_code, phone_number, post_code,
                                                    street, street_number)
@@ -32,7 +32,7 @@ VALUES ('Lódź', 2, '+48', '506304507', '05-120', 'Wysoka', 2),
        ('Warszawa', 12, '+48', '506304507', '05-120', 'Nowa', 12),
        ('Poznań', 10, '+48', '506304507', '05-120', 'Karetowa', 22),
        ('Lódź', 11, '+48', '506304507', '05-120', 'Wysoka', 32),
-       ('Warszawa', 13, '+48', '506304507', '05-120', 'Waska', 42),
+       ('Warszawa', 13, '+48', '506304507', '05-120', 'Wąska', 42),
        ('Poznań', 12, '+48', '506304507', '05-120', 'Wysoka', 42),
        ('Lódź', 12, '+48', '506304507', '05-120', 'Karetowa', 21),
        ('Lódź', 15, '+48', '506304507', '05-120', 'Wysoka', 23),
@@ -41,8 +41,8 @@ VALUES ('Lódź', 2, '+48', '506304507', '05-120', 'Wysoka', 2),
        ('Poznań', 12, '+48', '506304507', '05-120', 'Wysoka', 27),
        ('Lódź', 12, '+48', '506304507', '05-120', 'Karetowa', 22),
        ('Lódź', 15, '+48', '506304507', '05-120', 'Wysoka', 23),
-       ('Warszawa', 13, '+48', '506304507', '05-120', 'Waska', 42),
-       ('Lódź', 12, '+48', '506304507', '05-120', 'Waska', 23);
+       ('Warszawa', 13, '+48', '506304507', '05-120', 'Wąska', 42),
+       ('Lódź', 12, '+48', '506304507', '05-120', 'Wąska', 23);
 
 INSERT INTO conference_rooms_booking.user_details (date_of_birth, type_of_members)
 VALUES ('2021-10-29', 'USER'),
